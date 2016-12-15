f0ca287a085adfbab2d61773abb74f9ec0ca6760
Jacob van Eijk <jacob.vaneijk@gmail.com>
2016-12-15 17:00:14 +0100

    Add command line parsing.

--- src/main.c
+++ src/main.c
@@ -7,10 +7,63 @@
  * that was distributed with this source code.
  */
 
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <getopt.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+static struct option long_options[] = {
+    { "help",    no_argument, 0, 'h' },
+    { "version", no_argument, 0, 'v' },
+    { 0, 0, 0, 0 }
+};
 
-    return 0;
+static void display_help(const char *program) {
+    printf("Usage: %s [options] file\n"
+           "\n"
+           "Options:\n"
+           " -h, --help    Display this information.\n"
+           " -v, --version Display version information.\n"
+           "\n"
+           "For bug reports, please see <%s>.\n", program, PACKAGE_BUGREPORT);
+}
+
+static void display_version(void) {
+    printf("%s (%s) %s\nCopyright (c) 2016, Jacob van Eijk\n",
+        PACKAGE,
+        PACKAGE_NAME,
+        PACKAGE_VERSION);
+}
+
+int main(int argc, char **argv) {
+    int opt = 0;
+
+    while ((opt = getopt_long(argc, argv, "hv", long_options, NULL)) != -1) {
+        switch (opt) {
+            case 'h':
+                display_help(argv[0]);
+                return EXIT_SUCCESS;
+
+            case 'v':
+                display_version();
+                return EXIT_SUCCESS;
+
+            case '?':
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    if (optind >= argc) {
+        display_help(argv[0]);
+
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
