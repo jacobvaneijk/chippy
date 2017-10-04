5906e390feac526675246df33704d0879b538696
Jacob van Eijk <jacob.vaneijk@gmail.com>
2017-10-04 12:37:07 +0200

    Implement all opcodes up to and including 7XKK.

--- src/libchippy/chippy.c
+++ src/libchippy/chippy.c
@@ -24,6 +24,7 @@ void chippy_init(struct chippy *machine) {
     memcpy(machine->ram, fontset, sizeof(fontset));
     memset(machine->stack, 0, sizeof(machine->stack));
     machine->pc = PROGRAM_START;
+    machine->sp = 0;
 }
 
 int chippy_load_rom(struct chippy *machine, const char *rom) {
@@ -45,6 +46,8 @@ int chippy_step(struct chippy *machine) {
     uint16_t opcode = machine->ram[machine->pc] << 8
                     | machine->ram[machine->pc + 1];
 
+    machine->pc += 2;
+
     switch (opcode & 0xF000) {
         case 0x0000:
             switch (opcode & 0x00FF) {
@@ -61,6 +64,41 @@ int chippy_step(struct chippy *machine) {
                     printf("Invalid opcode 0x%X at address 0x%X.\n", opcode, machine->pc);
                     return EXIT_FAILURE;
             }
+            break;
+
+        case 0x1000: // JP: Jump to location NNN.
+            machine->pc = NNN(opcode);
+            break;
+
+        case 0x2000: // CALL: Call subroutine at NNN.
+            machine->stack[machine->sp++] = machine->pc;
+            machine->pc = NNN(opcode);
+            break;
+
+        case 0x3000: // SE: Skip next instruction if VX == KK.
+            if (machine->V[X(opcode)] == KK(opcode)) {
+                machine->pc += 2;
+            }
+            break;
+
+        case 0x4000: // SNE: Skip next instruction if VX != KK.
+            if (machine->V[X(opcode)] != KK(opcode)) {
+                machine->pc += 2;
+            }
+            break;
+
+        case 0x5000: // SE: Skip next instruction if VX == VY.
+            if (machine->V[X(opcode)] == machine->V[Y(opcode)]) {
+                machine->pc += 2;
+            }
+            break;
+
+        case 0x6000: // LD: Set VX = KK.
+            machine->V[X(opcode)] = KK(opcode);
+            break;
+
+        case 0x7000: // ADD: Set VX = VX + KK.
+            machine->V[X(opcode)] += KK(opcode);
             break;
 
         default:
--- tests/opcodes.c
+++ tests/opcodes.c
@@ -60,6 +60,95 @@ START_TEST(test_ret)
 }
 END_TEST
 
+START_TEST(test_jp)
+{
+    struct chippy *machine = chippy_create();
+
+    chippy_insert_opcode(machine, 0x1234, 0x200);
+    chippy_step(machine);
+
+    ck_assert_int_eq(machine->pc, 0x234);
+}
+END_TEST
+
+START_TEST(test_call)
+{
+    struct chippy *machine = chippy_create();
+
+    machine->stack[machine->sp++] = 0x234;
+
+    ck_assert_int_eq(machine->sp, 1);
+
+    chippy_insert_opcode(machine, 0x2345, 0x200);
+    chippy_step(machine);
+
+    ck_assert_int_eq(machine->sp, 2);
+    ck_assert_int_eq(machine->pc, 0x345);
+}
+END_TEST
+
+START_TEST(test_se_xkk)
+{
+    struct chippy *machine = chippy_create();
+
+    machine->V[2] = 0x12;
+
+    chippy_insert_opcode(machine, 0x3212, 0x200);
+    chippy_step(machine);
+
+    ck_assert_int_eq(machine->pc, 0x204);
+}
+END_TEST
+
+START_TEST(test_sne_xkk)
+{
+    struct chippy *machine = chippy_create();
+
+    machine->V[2] = 0x12;
+
+    chippy_insert_opcode(machine, 0x4299, 0x200);
+    chippy_step(machine);
+
+    ck_assert_int_eq(machine->pc, 0x204);
+}
+END_TEST
+
+START_TEST(test_se_xy)
+{
+    struct chippy *machine = chippy_create();
+
+    machine->V[2] = 0x12;
+    machine->V[3] = 0x12;
+
+    chippy_insert_opcode(machine, 0x5230, 0x200);
+    chippy_step(machine);
+
+    ck_assert_int_eq(machine->pc, 0x204);
+}
+END_TEST
+
+START_TEST(test_ld_xy)
+{
+    struct chippy *machine = chippy_create();
+
+    chippy_insert_opcode(machine, 0x6230, 0x200);
+    chippy_step(machine);
+
+    ck_assert_int_eq(machine->V[2], 0x30);
+}
+END_TEST
+
+START_TEST(test_add_xkk)
+{
+    struct chippy *machine = chippy_create();
+
+    chippy_insert_opcode(machine, 0x7120, 0x200);
+    chippy_step(machine);
+
+    ck_assert_int_eq(machine->V[1], 0x20);
+}
+END_TEST
+
 Suite *create_opcodes_suite(void) {
     Suite *suite = suite_create("Opcodes");
     TCase *chain = tcase_create("opcode tests");
@@ -68,6 +157,13 @@ Suite *create_opcodes_suite(void) {
 
     tcase_add_test(chain, test_cls);
     tcase_add_test(chain, test_ret);
+    tcase_add_test(chain, test_jp);
+    tcase_add_test(chain, test_call);
+    tcase_add_test(chain, test_se_xkk);
+    tcase_add_test(chain, test_sne_xkk);
+    tcase_add_test(chain, test_se_xy);
+    tcase_add_test(chain, test_ld_xy);
+    tcase_add_test(chain, test_add_xkk);
 
     return suite;
 }
